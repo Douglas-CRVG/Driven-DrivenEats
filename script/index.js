2de31fb30079a1d3cbdcc6bf0d008166eda1180d
Douglas <douglas.07.nascimento@gmail.com>
2021-10-28 19:58:53 -0300

    Active button

--- script/index.js
+++ script/index.js
@@ -1,22 +1,40 @@
+let foodSelected = 0;
+let drinkSelected = 0;
+let dessertSelected = 0;
+
+function activeButton(){
+    const button = document.querySelector("footer");
+    button.innerHTML='<button class="fechar-pedido"><h6>Fechar Pedido</h6></button>';
+}
+
 function select(selection, item){
     if (item !== null){
         item.classList.remove("selected");
     }
     selection.classList.add("selected");
+
+    const contador = foodSelected + drinkSelected + dessertSelected;
+
+    if(contador === 3){
+        activeButton();
+    }
 }
 
 function food(selection) {
+    foodSelected = 1;
     const item = document.querySelector(".food .selected");
     select(selection, item);
 }
 
 function drink(selection) {
+    drinkSelected = 1;
     const item = document.querySelector(".drink .selected");
     select(selection, item);
 }
 
 function dessert(selection) {
     const item = document.querySelector(".dessert .selected");
+    dessertSelected = 1;
     select(selection, item);
 }
 
